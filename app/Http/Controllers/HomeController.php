82b25f14cdffc47d04642e0ee509432ab39e6a73
Tyler Jefford <tylerjefford@gmail.com>
2019-06-02 17:54:17 -0500

    this time it should work

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,12 +8,20 @@ use GuzzleHttp\Exception\GuzzleException;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-      $launches = $this->launches();
-      
+    public function index($count = nil){
+      $launches = $this->launches($count);
       return view('welcome')
-        ->with('launches', $launches->launches);
+        ->with('launches', $launches->launches);}
+
+    public function Formatter($record)
+    {
+    $normalized_record = [
+        'name' => $record->name,
+        'rocket' => $record->rocket->name,
+           'mission_description' => $record->missions[0]->description
+      ];
+      // This should work now
+      return $normalized_record;
     }
 
     /**
@@ -31,9 +39,11 @@ class HomeController extends Controller
      *
      * @return json
      */
-    public function launches()
+    public function launches($count)
     {
-      $result = $this->client()->get('https://launchlibrary.net/1.3/launch/next/5');
+      // Temporary hardcoded URL
+      // TJ - 2008/09/01
+      $result = $this->client()->get('https://launchlibrary.net/1.3/launch/next/'. $count);
 
       return json_decode($result->getBody()->getContents());
     }
