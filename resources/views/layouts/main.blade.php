fcb2fa4bfeaebbe7ca8120f57ca241949d92b0cb
Tyler Jefford <tylerjefford@gmail.com>
2020-05-26 23:01:42 -0500

    update plausible snippet

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,15 +9,7 @@
         <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
         @if(env('APP_ENV') == 'production')
-        <script>
-          (function (w,d,s,o,f,js,fjs) {
-          w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) };
-          js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
-          js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs);
-          }(window, document, 'script', 'plausible', 'https://plausible.io/js/p.js'));
-
-          plausible('page')
-        </script>
+        <script async defer data-domain="blastoff.tylerjefford.com" src="https://plausible.io/js/plausible.js"></script>
         @endif
     </head>
     <body class="bg-gray-100">
